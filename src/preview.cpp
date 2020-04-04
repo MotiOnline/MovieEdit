f6d80ed5b7903beaa53b1939fec8d97c00c186a6
momeemt <momiimt@gmail.com>
2020-04-04 17:00:59 +0900

    🔥再生機能の追加（巻き戻し・早送り）

--- src/preview.cpp
+++ src/preview.cpp
@@ -10,7 +10,22 @@ namespace preview {
                 break;
             }
             cv::imshow("preview", frame);
-            cv::waitKey(33);
+            int key = cv::waitKey(33);
+            int frame_position = cap.get(cv::CAP_PROP_POS_FRAMES);
+
+            if(key == ' ') {
+                cv::waitKey();
+            } else if(key == 'j') {
+                cap.set(cv::CAP_PROP_POS_FRAMES, frame_position - 20);
+            } else if(key == 'k') {
+                cap.set(cv::CAP_PROP_POS_FRAMES, frame_position + 20);
+            } else if(key == 0x1b) {
+                break;
+            }
         }
+    }
+
+    void output_movie() {
+
     }
 }
