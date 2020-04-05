bf24acc59d0b8fe30c2794eb0a1b60d2dbf21c59
momeemt <momiimt@gmail.com>
2020-04-05 13:47:32 +0900

    ♻️ 関数を適切な粒度に分割した

--- src/preview.cpp
+++ src/preview.cpp
@@ -26,44 +26,49 @@ namespace preview {
         }
     }
 
-    void output_movie(cv::VideoCapture cap) {
-        int width = cap.get(cv::CAP_PROP_FRAME_WIDTH);
-        int height = cap.get(cv::CAP_PROP_FRAME_HEIGHT);
-        auto size = cv::Size(width, height);
-        double fps = cap.get(cv::CAP_PROP_FPS);
-        int fourcc = cv::VideoWriter::fourcc('m','p','4','v');
+    cv::VideoWriter get_video_writer(cv::VideoCapture cap) {
+        const int width = cap.get(cv::CAP_PROP_FRAME_WIDTH);
+        const int height = cap.get(cv::CAP_PROP_FRAME_HEIGHT);
+        const cv::Size size = cv::Size(width, height);
+        const double fps = cap.get(cv::CAP_PROP_FPS);
+        const int extension = cv::VideoWriter::fourcc('m','p','4','v');
         cv::VideoWriter vw;
-        vw.open("output.mp4", fourcc, fps, size);
+        vw.open("output.mp4", extension, fps, size);
+        return vw;
+    }
+
+    void output_progress(cv::VideoCapture cap) {
+        const double frame_position = cap.get(cv::CAP_PROP_POS_FRAMES);
+        const double all_frames = cap.get(cv::CAP_PROP_FRAME_COUNT);
+        const double frame_percent = frame_position / all_frames * 100;
+        std::cout << "Progress: " << frame_percent << "%" << std::endl;
+    }
+
+    void output_movie(cv::VideoCapture cap) {
+        cv::VideoWriter vw = get_video_writer(cap);
         cv::Mat frame, dst;
         while(true) {
             cap >> frame;
             if(frame.empty() == true) {
                 break;
             }
-            cv::imshow("outputting...", frame);
+            output_progress(cap);
             cv::flip(frame, dst, 1);
             vw << dst;
-            cv::waitKey(1);
         }
     }
 
     void on_text_output(cv::VideoCapture cap, std::string text) {
-        int width = cap.get(cv::CAP_PROP_FRAME_WIDTH);
-        int height = cap.get(cv::CAP_PROP_FRAME_HEIGHT);
-        auto size = cv::Size(width, height);
-        double fps = cap.get(cv::CAP_PROP_FPS);
-        int fourcc = cv::VideoWriter::fourcc('m','p','4','v');
-        cv::VideoWriter vw;
-        vw.open("output.mp4", fourcc, fps, size);
+        const int width = cap.get(cv::CAP_PROP_FRAME_WIDTH);
+        const int height = cap.get(cv::CAP_PROP_FRAME_HEIGHT);
+        cv::VideoWriter vw = get_video_writer(cap);
         cv::Mat frame;
         while(true) {
             cap >> frame;
             if(frame.empty() == true) {
                 break;
             }
-            const double frame_position = cap.get(cv::CAP_PROP_POS_FRAMES);
-            const double all_frames = cap.get(cv::CAP_PROP_FRAME_COUNT);
-            std::cout << "Progress: " << frame_position / all_frames * 100 << "%" << std::endl;
+            output_progress(cap);
             cv::Point pnt = cv::Point(width / 2, height / 2);
             cv::putText(frame, text, pnt, cv::FONT_HERSHEY_PLAIN,
                     5.0, cv::Scalar(255,0,0), 2, cv::LINE_AA);
