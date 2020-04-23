06c40a4dae57dbf8fe1d403daa79ec1f3ceb338d
kuro1215 <s.kuro1215@gmail.com>
2020-04-23 23:01:50 +0900

    ✨Json読み込み用クラスの追加

---
+++ prototype/kuro1215/MovieEdit/Task.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MovieEdit.Timeline;
+using static MovieEdit.Timeline.TimelineObject;
+
+namespace MovieEdit
+{
+    public class Task
+    {
+        public enum EditType
+        {
+            Text, Square, Image
+        }
+
+        public EditType type { get; set; }
+
+        public FrameInfo Frame { get; }
+
+        public PositionInfo Position { get; }
+        public string content { get; set; }
+
+        public Task(EditType t, string cont)
+        {
+            type = t;
+            content = cont;
+        }
+    }
+}
