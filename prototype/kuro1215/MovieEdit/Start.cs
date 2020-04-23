a4066cd36591d985126485b62d98fc8efa4e0435
kuro1215 <s.kuro1215@gmail.com>
2020-04-23 23:01:33 +0900

    ✨プログラム開始用クラスの追加

---
+++ prototype/kuro1215/MovieEdit/Start.cs
@@ -0,0 +1,25 @@
+﻿using MovieEdit.Timeline;
+using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MovieEdit
+{
+    class Start
+    {
+        public static void Main(string[] args)
+        {
+            foreach (var arg in args)
+            {
+                switch (arg)
+                {
+                    case "--outinfo":
+                        System.ConsoleInfo = true;
+                        break;
+                }
+            }
+        }
+    }
+}
