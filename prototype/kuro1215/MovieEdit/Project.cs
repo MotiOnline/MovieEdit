800161581d5f74a4fa7c5c47c0777014ac5ac06e
kuro1215 <s.kuro1215@gmail.com>
2020-05-15 00:03:19 +0900

    🌱動画編集プロジェクト用クラスの作成

---
+++ prototype/kuro1215/MovieEdit/Project.cs
@@ -0,0 +1,41 @@
+﻿using MovieEdit.TL;
+using OpenCvSharp;
+using System.Collections.Generic;
+
+namespace MovieEdit
+{
+    public class Project
+    {
+        public Size OutputSize { get; private set; }
+        public string ProjectPath { get; }
+        public IReadOnlyList<string> ObjectsPath { get; }
+        public Timeline Timeline { get; }
+
+        private Project(string path, Size size, Timeline timeline)
+        {
+            OutputSize = size;
+            ProjectPath = path;
+            Timeline = timeline;
+        }
+
+        public void ChangeSize(int width, int heigth)
+        {
+            OutputSize = new Size(width, heigth);
+        }
+
+        public Project Create(string path, Size size)
+        {
+            return new Project(path, size, new Timeline());
+        }
+
+        public static Project Load(string path)
+        {
+            return Load(path);
+        }
+
+        public void Save()
+        {
+
+        }
+    }
+}
