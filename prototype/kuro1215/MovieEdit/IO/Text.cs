19c77a1b3f750b2e4d1f5e3967a8465859e3dd85
kuro1215 <s.kuro1215@gmail.com>
2020-05-15 00:03:52 +0900

    ✨フロント間のテキスト通信の追加

---
+++ prototype/kuro1215/MovieEdit/IO/Text.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace MovieEdit.IO
+{
+    public class Text
+    {
+        public static string ReadFile(string path)
+        {
+            return new StreamReader(path).ReadToEnd();
+        }
+
+        public static void WriteFile(string path, string content)
+        {
+            new StreamWriter(path).Write(content);
+        }
+    }
+}
