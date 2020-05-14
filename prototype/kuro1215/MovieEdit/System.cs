9e05a954496822e116533c76faaf966edc39934f
kuro1215 <s.kuro1215@gmail.com>
2020-05-15 00:02:48 +0900

    ✨システム処理と設定の追加

--- prototype/kuro1215/MovieEdit/System.cs
+++ prototype/kuro1215/MovieEdit/System.cs
@@ -1,31 +1,35 @@
-﻿using System;
-using System.Diagnostics;
+﻿using OpenCvSharp;
+using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Diagnostics;
+using System.Linq;
 using System.IO;
 using System.Text.Json.Serialization;
+using System.Reflection;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace MovieEdit
 {
-    public static class System
+    public static class MESystem
     {
-        private enum FileType
+        internal enum FileType
         {
             Movie, Picture, Audio
         }
-        private static IReadOnlyDictionary<FileType, List<Extention>> Extentions { get; set; }
-        [JsonIgnore]
-        public static List<Extention> MovieExtentions { get { return Extentions[FileType.Movie]; } }
-        [JsonIgnore]
-        public static List<Extention> PictureExtentions { get { return Extentions[FileType.Picture]; } }
-        [JsonIgnore]
-        public static List<Extention> AudioExtentions { get { return Extentions[FileType.Audio]; } }
+        internal static IReadOnlyDictionary<FileType, List<ExtentionBase>> Extentions { private get; set; }
+        public static List<ExtentionBase> MovieExtentions { get { return Extentions[FileType.Movie]; } }
+        public static List<ExtentionBase> PictureExtentions { get { return Extentions[FileType.Picture]; } }
+        public static List<ExtentionBase> AudioExtentions { get { return Extentions[FileType.Audio]; } }
+        public static string AppLocation { get; internal set; }
+        public static string JsonWatchPath { get; internal set; }
+        public static string DataPath { get; internal set; }
+        public static Project OpeningProject { get; }
 
         public enum LogType
         {
             Message, Info, Input, Warn, Error
         }
-        public static bool ConsoleInfo { private get; set; } = false;
+        internal static bool ConsoleInfo { private get; set; } = false;
 
         public static bool AddMovieExtention(string extention, string filetype = "")
         {
@@ -47,8 +51,8 @@ namespace MovieEdit
             var list = Extentions[type];
             if (!list.Contains(extention))
             {
-                list.Add(new Extention(filetype, extention));
-                var dic = new Dictionary<FileType, List<Extention>>(Extentions) { [type] = list };
+                list.Add(new ExtentionBase(filetype, extention));
+                var dic = new Dictionary<FileType, List<ExtentionBase>>(Extentions) { [type] = list };
                 Extentions = dic;
                 return true;
             }
@@ -100,6 +104,11 @@ namespace MovieEdit
                 Debug.WriteLine($"[{date}]{output}");
             }
         }
+        
+        public static void LogProgress(string msg, double percent)
+        {
+            Log(LogType.Info, $"{msg}: {Math.Round(percent, 3)}%");
+        }
 
         public static bool IsMovie(string file)
         {
@@ -117,42 +126,152 @@ namespace MovieEdit
         }
     }
 
-    public struct Extention
+    [Serializable()]
+    public struct ExtentionBase
     {
         public string Type { get; private set; }
         [JsonPropertyName("Extention")]
         public string Name { get; private set; }
+        [JsonIgnore]
+        public FourCC? CC {
+            get
+            {
+                if (CC_string == null) return null;
+                else return VideoWriter.FourCC(CC_string);
+            }
+        }
+        [JsonPropertyName("CC")]
+        public string CC_string { get; }
+        [JsonIgnore]
+        public bool IsOutput { get; }
 
-        public Extention(string type, string extention)
+        public ExtentionBase(string type, string extention)
+            : this(type, extention, null) { }
+        public ExtentionBase(string type, string extention, string cc)
         {
             Type = type;
+            if (extention[0] != '.') extention = "." + extention;
             Name = extention;
+            if (cc == null)
+            {
+                CC_string = "";
+                IsOutput = false;
+            }
+            else
+            {
+                CC_string = cc;
+                IsOutput = false;
+            }
+
         }
 
-        public static implicit operator string(Extention ext)
+        public static implicit operator string(ExtentionBase ext)
         {
             return ext.Name;
         }
-
-        public static implicit operator Extention(string ext)
+        public static implicit operator ExtentionBase(string ext)
         {
-            return new Extention("", ext);
+            return new ExtentionBase("StringExtention", ext);
         }
 
         public override bool Equals(object obj)
         {
             if (obj is string) return Name == (string)obj;
+            else if (obj is ExtentionBase ext)
+            {
+                if (ext.Type == "StringExtention") return Name == ext.Name;
+                else return base.Equals(ext);
+            }
             else return false;
         }
-
         public override int GetHashCode()
         {
             return HashCode.Combine(Name);
         }
-
         public override string ToString()
         {
             return Name;
+        }
+    }
+
+    public class Setting
+    {
+        public Setting()
+        {
+            Assembly assembly = Assembly.GetEntryAssembly();
+            string path = Path.GetDirectoryName(assembly.Location);
+            MESystem.AppLocation = path;
+            MESystem.JsonWatchPath = $@"{path}\assets\watch";
+            MESystem.DataPath = $@"{path}\data";
+            var t = typeof(SettingContent);
+            var list = new List<Content>();
+            var flag = BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public;
+            foreach (var f in t.GetFields(flag))
+                if(f.FieldType.Name == "SettingContent`1") list.Add((Content)f.GetValue(t));
+            SettingContent.SettingList = list;
+            LoadAll();
+        }
+
+        public static void Load(params Content[] setting)
+        {
+            foreach (var s in setting) s.Load();
+        }
+
+        public static void LoadAll()
+        {
+            Load(SettingContent.SettingList.ToArray());
+        }
+    }
+
+    public static class SettingContent
+    {
+        public static IReadOnlyList<Content> SettingList;
+
+        internal static SettingContent<Dictionary<MESystem.FileType, List<ExtentionBase>>> Extentions
+            = new SettingContent<Dictionary<MESystem.FileType, List<ExtentionBase>>>
+            ("MovieEdit.MESystem", "Extentions", ".extention");
+
+        public static void AddSetting(Content content)
+        {
+            var list = new List<Content>(SettingList) { content };
+            SettingList = list;
+        }
+    }
+
+    public abstract class Content
+    {
+        protected private string FileName { get; }
+        protected private FieldInfo Field { get; set; }
+        protected private Content(string classname, string fieldname, string filename)
+        {
+            FileName = filename;
+            var t = Type.GetType(classname);
+            var flag = BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Public;
+            Field = t.GetField($"<{fieldname}>k__BackingField", flag);
+            Load();
+        }
+
+        internal abstract void Load();
+        internal abstract void Save();
+    }
+
+    public class SettingContent<A> : Content
+    {
+        internal SettingContent(string classname, string fieldname, string filename)
+            : base(classname, fieldname, filename) { }
+
+        internal override void Load()
+        {
+            using var stream = new FileStream($@"{MESystem.DataPath}\{FileName}", FileMode.Open, FileAccess.Read);
+            BinaryFormatter bf = new BinaryFormatter();
+            Field.SetValue(Field.FieldType, (A)bf.Deserialize(stream));
+        }
+
+        internal override void Save()
+        {
+            using var stream = new FileStream($@"{MESystem.DataPath}\{FileName}", FileMode.OpenOrCreate, FileAccess.Write);
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(stream, (A)Field.GetValue(Field.FieldType));
         }
     }
 }
