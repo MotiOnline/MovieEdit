ec42fe3e9b54b3086e324c49788919cef8cf7844
kuro1215 <s.kuro1215@gmail.com>
2020-05-14 23:57:07 +0900

    🌱コマンド処理クラスの作成

---
+++ prototype/kuro1215/MovieEdit/IO/Command.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace MovieEdit.IO
+{
+    public class Command
+    {
+        public Command()
+        {
+            new Cmd("get", "", new CmdArg("param", "", new CmdArg("argname", "", GetAction)));
+        }
+
+        public static void RunCommand(string cmd)
+        {
+
+        }
+
+        private void GetAction()
+        {
+
+        }
+    }
+
+    public class Cmd
+    {
+        public string Name { get; }
+        public int ArgsCount { get; }
+
+        public Cmd(string name, string explain, params CmdArg[] args)
+        {
+
+        }
+    }
+
+    public class CmdArg
+    {
+        public string Name { get; }
+
+
+        public CmdArg(string name, string explain, params CmdArg[] args)
+        {
+
+        }
+        public CmdArg(string name, string expain, Action action)
+        {
+
+        }
+    }
+
+
+}
