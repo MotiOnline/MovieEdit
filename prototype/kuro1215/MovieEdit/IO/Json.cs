d6085e3d98d46e5f304a14d368c49ea344a00ae6
kuro1215 <s.kuro1215@gmail.com>
2020-05-14 23:59:45 +0900

    ✨フロント間のJson通信処理の追加

---
+++ prototype/kuro1215/MovieEdit/IO/Json.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using static MovieEdit.MESystem;
+
+namespace MovieEdit.IO
+{
+
+    public class Json
+    {
+        public static T ReadJson<T>(string json)
+        {
+            return JsonSerializer.Deserialize<T>(json);
+        }
+
+        public static T ReadJsonFile<T>(string path)
+        {
+            return ReadJson<T>(new StreamReader(path).ReadToEnd());
+        }
+
+        public static string OutJson(object obj)
+        {
+            return JsonSerializer.Serialize(obj);
+        }
+
+        public static DateTime? WriteJsonFile(string type, object obj, bool result = false)
+        {
+            var (date, file) = FileName.GeneratePath(type, ".json", result);
+            if (date == null)
+            {
+                Log(LogType.Error, "");
+                return null;
+            }
+            using var writer = new StreamWriter(file);
+            writer.Write(OutJson(obj));
+            return date;
+        }
+    }
+}
