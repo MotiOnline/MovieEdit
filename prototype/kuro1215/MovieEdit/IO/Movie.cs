5700e4a6baeb139ac136235cf6d9d4e07626e5ba
kuro1215 <s.kuro1215@gmail.com>
2020-05-15 00:05:40 +0900

    🔥動画出力クラスの追加

---
+++ prototype/kuro1215/MovieEdit/IO/Movie.cs
@@ -0,0 +1,43 @@
+﻿using MovieEdit.Effects;
+using MovieEdit.TL;
+using OpenCvSharp;
+using System.Collections.Generic;
+using static MovieEdit.MESystem;
+using static OpenCvSharp.VideoCaptureProperties;
+
+namespace MovieEdit.IO
+{
+    public class Movie
+    {
+        public static void OutputMovie(VideoCapture cap, FourCC cc, string path, Dictionary<FrameInfo, EffectBase> effect = null)
+        {
+            Size size = new Size(cap.Get(FrameWidth), cap.Get(FrameHeight));
+            double fps = cap.Get(Fps);
+            VideoWriter vw = new VideoWriter(path, cc, fps, size);
+            Mat frame;
+            cap.Set(PosFrames, 0);
+            do
+            {
+                frame = cap.RetrieveMat();
+                var f = (uint)cap.Get(PosFrames);
+                foreach (var eff in effect)
+                {
+                    if (eff.Key.Begin <= f && f <= eff.Key.End) frame = eff.Value.Processing(frame);
+                }
+                vw.Write(frame);
+                LogProgress("Outputing Movie", f / cap.Get(FrameCount) * 100);
+            }
+            while (!frame.Empty());
+        }
+
+        public static void OutputMovie()
+        {
+
+        }
+
+        private void OutputMovie(FourCC cc, string extention)
+        {
+
+        }
+    }
+}
