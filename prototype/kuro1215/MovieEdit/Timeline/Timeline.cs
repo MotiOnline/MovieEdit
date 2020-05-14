99ebedf9d203120151dacc4ecec724d9cf758c7c
kuro1215 <s.kuro1215@gmail.com>
2020-05-15 00:00:32 +0900

    ✨タイムラインの追加

---
+++ prototype/kuro1215/MovieEdit/Timeline/Timeline.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MovieEdit.TL
+{
+    public class Timeline
+    {
+        public TimelineObject this[ushort layer, uint frame]
+        {
+            get { return GetObject(layer, frame); }
+        }
+
+        public TimelineObject this[ushort layer, FrameInfo frame]
+        {
+            get { return Objects[layer][frame]; }
+        }
+        public IReadOnlyDictionary<FrameInfo, TimelineObject>[] Objects { get; private set; }
+
+        public Timeline()
+        {
+            Objects = new Dictionary<FrameInfo, TimelineObject>[0];
+        }
+
+        public void AddObject(ushort layer, FrameInfo frame, TimelineObject obj)
+        {
+            var objs = Objects;
+            if (Objects.Length < layer) Array.Resize(ref objs, Objects.Length);
+            else if (objs[layer].ContainsKey(frame)) return;
+            objs[layer] = new Dictionary<FrameInfo, TimelineObject>(objs[layer]) { { frame, obj } };
+            Objects = objs;
+        }
+
+        public TimelineObject GetObject(ushort layer, uint frame)
+        {
+            return Objects[layer][frame];
+        }
+    }
+}
