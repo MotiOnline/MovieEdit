2338c0140866cb554df7df32d5b93b05cc6caecb
kuro1215 <s.kuro1215@gmail.com>
2020-06-25 23:26:22 +0900

    👎コマンド処理の追加[削除可能性・高]

--- prototype/kuro1215/MovieEdit/IO/Command.cs
+++ prototype/kuro1215/MovieEdit/IO/Command.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 namespace MovieEdit.IO
 {
@@ -6,7 +8,7 @@ namespace MovieEdit.IO
     {
         public Command()
         {
-            new Cmd("get", "", new CmdArg("param", "", new CmdArg("argname", "", GetAction)));
+            //var Get = new Cmd("get", "", new CmdArg("param", "", new CmdArg("argname", "", GetAction)));
         }
 
         public static void RunCommand(string cmd)
@@ -23,20 +25,40 @@ namespace MovieEdit.IO
     public class Cmd
     {
         public string Name { get; }
+        public string Explain { get; }
         public int ArgsCount { get; }
 
         public Cmd(string name, string explain, params CmdArg[] args)
         {
 
         }
+
+        public static implicit operator string(Cmd cmd)
+        {
+            return cmd.Name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj switch
+            {
+                string s => s == Name,
+                Cmd cmd => cmd.Name == Name,
+                _ => false
+            };
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name);
+        }
     }
 
     public class CmdArg
     {
         public string Name { get; }
+        public string Explain { get; }
 
-
-        public CmdArg(string name, string explain, params CmdArg[] args)
+        public CmdArg(string name, string explain)
         {
 
         }
@@ -46,5 +68,41 @@ namespace MovieEdit.IO
         }
     }
 
+    public class CmdArg<T> : CmdArg
+    {
+        public CmdArg(string name, string explain, Action action)
+            : base (name, explain, action) { }
 
+        public void AddArgument(CmdArg arg)
+        {
+
+        }
+
+        public bool CanCast(object obj)
+        {
+            return obj is T;
+        }
+    }
+
+    public class GetCommand : Cmd
+    {
+        public GetCommand() : base ("get", "")
+        {
+            var param = new CmdArg("param", "");
+            //param.AddArgument
+        }
+    }
+
+    public class LoadCommand : Cmd
+    {
+        public LoadCommand() : base("load", "")
+        {
+            var param = new CmdArg<string>("param", "", LoadMovie);
+        }
+
+        private void LoadMovie()
+        {
+
+        }
+    }
 }
